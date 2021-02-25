92c8b86af227bd85542977ffcefa143ddb82b3c1
mainu007 <mainudev.co@gmail.com>
2021-02-25 16:54:38 +0600

    added react-navigation-material-bottom-tabs

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -9,6 +9,8 @@ import MealDetailScreen from '../screens/MealDetailScreen';
 import FavoriteScreen from '../screens/FavoriteScreen';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
+import {Platform} from 'react-native';
 
 const MealsNavigator = createStackNavigator(
   {
@@ -33,36 +35,43 @@ const MealsNavigator = createStackNavigator(
   },
 );
 
-const MealTabNavigator = createBottomTabNavigator(
-  {
-    Meals: {
-      screen: MealsNavigator,
-      navigationOptions: {
-        tabBarIcon: (tabInfo) => {
-          return (
-            <MaterialIcons
-              name="restaurant"
-              color={tabInfo.tintColor}
-              size={25}
-            />
-          );
-        },
-      },
-    },
-    Favorites: {
-      screen: FavoriteScreen,
-      navigationOptions: {
-        tabBarIcon: (tabInfo) => {
-          return <AntDesign name="star" size={25} color={tabInfo.tintColor} />;
-        },
+const tabNavigatorConfig = {
+  Meals: {
+    screen: MealsNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return (
+          <MaterialIcons
+            name="restaurant"
+            color={tabInfo.tintColor}
+            size={25}
+          />
+        );
       },
     },
   },
-  {
-    tabBarOptions: {
-      activeTintColor: Colors.accent,
+  Favorites: {
+    screen: FavoriteScreen,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return <AntDesign name="star" size={25} color={tabInfo.tintColor} />;
+      },
     },
   },
-);
+};
+
+const MealTabNavigator =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabNavigatorConfig, {
+        activeColor: '#fff',
+        barStyle: {
+          backgroundColor: Colors.primary,
+        },
+      })
+    : createBottomTabNavigator(tabNavigatorConfig, {
+        tabBarOptions: {
+          activeTintColor: Colors.accent,
+        },
+      });
 
 export default createAppContainer(MealTabNavigator);
